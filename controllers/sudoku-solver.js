b496fc16ef906b05dbcad8d9bb4344d8ba27f74b
KarimAmador <karimalejandroAC@gmail.com>
2024-08-02 12:01:25 -0600

    feat: add validate function code

--- controllers/sudoku-solver.js
+++ controllers/sudoku-solver.js
@@ -1,6 +1,13 @@
 class SudokuSolver {
 
+  /**
+   * Function to validate the puzzle string.
+   * @param {String} puzzleString
+   */
   validate(puzzleString) {
+    if (puzzleString.length != 81) return { error: 'Expected puzzle to be 81 characters long' };
+    if (!puzzleString.match(/^[1-9.]+$/)) return { error: 'Invalid characters in puzzle' };
+    return true;
   }
 
   checkRowPlacement(puzzleString, row, column, value) {
