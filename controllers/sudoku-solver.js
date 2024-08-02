176d5bf8f8b2f4b5b5e323d6a7c56da4a7f4fc1e
KarimAmador <karimalejandroAC@gmail.com>
2024-08-02 12:02:56 -0600

    feat: add checkRowPlacement function code

--- controllers/sudoku-solver.js
+++ controllers/sudoku-solver.js
@@ -10,8 +10,20 @@ class SudokuSolver {
     return true;
   }
 
+  /**
+   * Function to check valid row placement for a value
+   * @param {String} puzzleString 
+   * @param {Number} row 
+   * @param {Number} column 
+   * @param {String} value 
+   */
   checkRowPlacement(puzzleString, row, column, value) {
+    const rowString = puzzleString.slice((row - 1) * 9, (row - 1) * 9 + 9);
+    console.log(rowString);
 
+    if (rowString.includes(value)) return false;
+    
+    return true;
   }
 
   checkColPlacement(puzzleString, row, column, value) {
