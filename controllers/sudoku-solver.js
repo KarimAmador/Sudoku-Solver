7f5f19f88cec9fa9aba73622339d1478013fa3dd
KarimAmador <karimalejandroAC@gmail.com>
2024-08-07 11:48:00 -0600

    feat: add solve code and checkValidPlacement method

--- controllers/sudoku-solver.js
+++ controllers/sudoku-solver.js
@@ -64,8 +64,57 @@ class SudokuSolver {
     return !region.includes(value);
   }
 
+  /**
+   * Function to check valid placement for a value
+   * @param {String} puzzleString 
+   * @param {Number} nextEmpty 
+   * @param {String} value 
+   * @returns {Boolean}
+   */
+  checkValidPlacement(puzzleString, nextEmpty, value) {
+    const row = Math.floor(nextEmpty / 9);
+    const col = nextEmpty % 9;
+
+    return ([
+      this.checkRowPlacement(puzzleString, row, value),
+      this.checkColPlacement(puzzleString, col, value),
+      this.checkRegionPlacement(puzzleString, row, col, value)
+    ].every((item) => item));
+  }
+
+  /**
+   * Function to solve a sudoku
+   * @param {String} puzzleString 
+   */
   solve(puzzleString) {
-    
+    if (puzzleString.length < 81) {
+      return { error: 'Expected puzzle to be 81 characters long' };
+    }
+    if (!puzzleString.match(/^[1-9.]+$/)) {
+      return { error: 'Invalid characters in puzzle' };
+    }
+
+    let puzzle = puzzleString.split('');
+
+    return this._solve(puzzle) ? { solution: puzzle.join('') } : { error: 'Puzzle cannot be solved' };
+  }
+
+  _solve(puzzle) {
+    let nextEmpty = puzzle.findIndex((item) => item === '.');
+
+    if (nextEmpty === -1) return true;
+
+    for (let value = 1; value < 10; value++) {
+      if (this.checkValidPlacement(puzzle.join(''), nextEmpty, `${value}`)) {
+        puzzle[nextEmpty] = `${value}`;
+        if (this._solve(puzzle)) {
+          return true;
+        }
+
+        puzzle[nextEmpty] = '.';
+      }
+    }
+    return false;
   }
 }
 
